d0f04f63dee7d8f4e858149b3511609ee469980a
sirio95 <sirio.iasevoli@gmail.com>
2023-01-09 18:18:31 +0100

    correzione index.php (problema intestazione file)

--- index.php
+++ index.php
@@ -1,29 +1,58 @@
+<?php
+$title = "Alcune parole verranno censurate";
+$paragraph = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur inventore autem eaque perferendis hic aperiam? Qui inventore animi nisi! Odio porro harum, minus temporibus accusamus reprehenderit fuga itaque dolorum eum!";
+$parola = $GET_["parola"];
+$censura = "***";
+$lunghezza = strlen($paragraph);
+
+?>
+<!DOCTYPE html
+    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head lang="<?php echo $str_language; ?>" xml:lang="<?php echo $str_language; ?>">
+    <meta http-equiv="content-type" content="text/html; charset-utf-8" />
+    <title>BadWords</title>
+    <style type="text/css">
+    </style>
+</head>
 <html>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Badwords</title>
-</head>
+
 
 <body>
     <h1>
         <?php
-        $title = "Alcune parole verranno censurate";
         echo $title;
         ?>
     </h1>
-    <p>
-        <?php
-        $paragraph = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur inventore autem eaque perferendis hic aperiam? Qui inventore animi nisi! Odio porro harum, minus temporibus accusamus reprehenderit fuga itaque dolorum eum!";
-        echo $paragraph;
-        $parola = $GET_["parola"];
-        $censura = "***";
-        $censurato = str_replace($censura, $parola, $paragraph);
-        echo $censurato;
-        ?>
-    </p>
+    <section id="trasparente">
+        <p>
+            <?php
+            echo $paragraph;
+            ?>
+        </p>
+        <h3>
+            <?php
+            echo "Il paragrafo contiene " . $lunghezza . " " . "parole";
+            ?>
+        </h3>
+    </section>
+    <section id="censurato">
+        <p>
+            <?php
+            echo str_replace($censura, $parola, $paragraph);
+            ?>
+        </p>
+        <h3>
+            <?php
+            echo "Il paragrafo contiene sempre " . $lunghezza . " " . "parole";
+            ?>
+        </h3>
+
+    </section>
+
+
 </body>
 
 </html>
