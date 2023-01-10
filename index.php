a9a412903e0b429187373ee06ee6ceae06734482
sirio95 <sirio.iasevoli@gmail.com>
2023-01-10 09:59:42 +0100

    correzione organizzazione intestazione documento

--- index.php
+++ index.php
@@ -1,24 +1,19 @@
-<?php
-$title = "Alcune parole verranno censurate";
-$paragraph = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur inventore autem eaque perferendis hic aperiam? Qui inventore animi nisi! Odio porro harum, minus temporibus accusamus reprehenderit fuga itaque dolorum eum!";
-$parola = $GET_["parola"];
-$censura = "***";
-$lunghezza = strlen($paragraph);
+<!DOCTYPE html>
+<html lang="en">
 
-?>
-<!DOCTYPE html
-    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
-<head lang="<?php echo $str_language; ?>" xml:lang="<?php echo $str_language; ?>">
-    <meta http-equiv="content-type" content="text/html; charset-utf-8" />
-    <title>BadWords</title>
-    <style type="text/css">
-    </style>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Badwords</title>
+    <?php
+    $title = "Alcune parole verranno censurate";
+    $paragraph = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur inventore autem eaque perferendis hic aperiam? Qui inventore animi nisi! Odio porro harum, minus temporibus accusamus reprehenderit fuga itaque dolorum eum!";
+    $parola = $GET_["parola"];
+    $censura = "***";
+    $lunghezza = strlen($paragraph);
+    ?>
 </head>
-<html>
-
-
 
 <body>
     <h1>
@@ -51,8 +46,6 @@ $lunghezza = strlen($paragraph);
         </h3>
 
     </section>
-
-
 </body>
 
 </html>
